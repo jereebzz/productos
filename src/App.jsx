15dae84bca2026bb762631ef69b88f89eee1a9e3
jere <jereebaezz@gmail.com>
2024-05-29 19:29:16 -0300

    se agrego una funcion para mostrar productos y categorias desde una api

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,73 @@
-import { Component} from 'react'
-import axios from "axios"
-import './App.css'
+import { Component } from 'react';
+import axios from 'axios';
+import './App.css';
 
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categoria_id:"",
+      categorias: [],
+      productos: []
+    };
+  }
 
+  buscarCategorias() {
+    const url = 'https://productos.ctpoba.edu.ar/api/categorias';
+    // const url = 'http://10.0.4.103:3000/api/categorias';
+    axios.get(url)
+      .then((resp) => {
+        console.log(resp.data);
+        this.setState({ categorias: resp.data.categorias });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
+  buscarProductos(categoria_id) {
+    const url = 'https://productos.ctpoba.edu.ar/api/productos';
+    // const url = 'http://10.0.4.103:3000/api/productos';
+    const config={
+      params:{categoria_id}
+    }
+    axios.get(url, config)
+      .then((resp) => {
+        console.log(resp.data);
+        this.setState({ productos: resp.data.productos });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-export default class App extends Component{
-
-constructor(props){
-super(props);
-this.state = {
-  categorias:[]
-};
-}
-  
-
-buscarCategorias(){
-  const url = "http://productos.ctpoba.edu.ar/api/categorias"
-  axios.get(url)
-  .then ((resp) => {
-this.setState({ categorias: resp.data.categorias});
-  })
-
-  .then ((error) => {
-console.log(error);
-  });
-}
-
-render(){
-  return(
-<div>
-  <span>APP</span>
-  <input type="button" value="Buscar" onClick={this.buscarCategorias()}
-   />
-
-   <select>
-{this.state.categorias.map((categoria, index)=> (
-
-  <option key={index} value={categoria.id}>
-  </option>
-))}
-   </select>
-
-</div>
-);
-}
+  render() {
+    return (
+      <div>
+        <input
+          type="button"
+          value="Buscar categorias"
+          onClick={() => this.buscarCategorias()}
+        />
+        <select
+          value={this.state.categoria_id}
+          onChange={(e) => this.setState({categoria_id:e.target.value})}
+        >
+          {this.state.categorias.map((cat, i) =>
+          <option key={cat.id} value={cat.id} > {cat.nombre}</option>
+    )}
+    </select>
+    <input
+      type="button"
+      value="Buscar productos"
+      onClick={() => this.buscarProductos(this.state.categoria_id)}
+    />
+    <div className='tarjetas'>
+    {this.state.productos.map((prod,id)=>
+      <h2 key={id}> {prod.nombre} </h2>
+    )}
+    </div>
+    </div>
+    )
+  }
 }
